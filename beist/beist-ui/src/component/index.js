bf29a6062f008c032aacb65f50bf862b5bff9a53
ylzhang96 <ylzhang96@163.com>
2017-08-23 22:35:24 +0800

    [zyl] hello world ui

---
+++ beist/beist-ui/src/component/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {  } from 'react-bootstrap';    // import bootstrap framework
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
+
+export default Index;
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@
 'Beist' is a Java Web & Android Application for memorizing English words.
 
 ## Help
+* npm
+* [安装reactApp](https://github.com/facebookincubator/create-react-app)
+* 
